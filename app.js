bc753864246537ceeb0cc2f3fdb8b2d1585f95b0
Joe Almanza <joealmanza2113@gmail.com>
2020-08-19 15:42:32 -0600

    gettin there

--- app.js
+++ app.js
@@ -1,35 +1,59 @@
 // let weapons = ["rock", "paper", "scissors"]
 
+
+
 function choose(playerChoice){  
-  let computerChoice = "rock"
+  let result = document.getElementById("result")
+  let computerChoice = getComputerChoice();
   if(playerChoice == computerChoice){
     console.log("tie")
+    result.innerText = `
+      The computer chose ${computerChoice}, so its a tie!
+      `
   }
   else if(playerChoice == "scissors"){
     if(computerChoice == "rock"){
       console.log("lose")
+      result.innerText = `
+      The computer chose ${computerChoice}, so you lose!
+      `
     }
     else{
       console.log("win")
+      result.innerText = `
+      The computer chose ${computerChoice}, so you win!
+      `
     }
   }
   else if(playerChoice == "paper"){
     if(computerChoice == "rock"){
       console.log("win")
+      result.innerText = `
+      The computer chose ${computerChoice}, so you win!
+      `
     }
     else{
       if(computerChoice == "scissors"){
         console.log("lose")
+        result.innerText = `
+      The computer chose ${computerChoice}, so you lose!
+      `
       }
     }
   }
   else{
     if(computerChoice == "paper"){
       console.log("lose")
+      result.innerText = `
+      The computer chose ${computerChoice}, so you lose!
+      `
     }
     else{
-      if(computerChoice == "rock"){
+      if(computerChoice == "scissors"){
         console.log("win")
+        result.innerText = `
+      The computer chose ${computerChoice}, so you win!
+      `
       }
     }
   }
@@ -38,13 +62,13 @@ function choose(playerChoice){
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3)
   if(choice == 0){
-    let computerChoice = "rock"
+    return "rock"
   }
   else if(choice == 1){
-    let computerChoice = "paper"
+    return "paper"
   }
   else{
-    let computerChoice = "scissors"
+    return "scissors"
   }
   // let weapon = weapons[index]
   // play(weapons)
